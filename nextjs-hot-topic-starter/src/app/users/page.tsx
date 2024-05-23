dbd2ec258b37e12b255340b98e6cfdeaee4370ad
ThangNP <phuocthang238@gmail.com>
2024-05-24 00:47:07 +0700

    night 23/5

--- nextjs-hot-topic-starter/src/app/users/page.tsx
+++ nextjs-hot-topic-starter/src/app/users/page.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import UserTable from "@/components/user-table";
+const page = async (props: any) => {
+  const limit = 1;
+  const page = props?.searchParams?.page ?? 1;
+  const res = await fetch(
+    `http://localhost:8000/users?_page=${page}&_limit=${limit}`
+  );
+  const data = await res.json();
 
-const page = () => {
-  return <div>User</div>;
+  return (
+    <div>
+      <h1>users</h1>
+      <UserTable users={data ? data : []} />
+    </div>
+  );
 };
 
 export default page;
--- nextjs-hot-topic-starter/src/components/user-table.tsx
+++ nextjs-hot-topic-starter/src/components/user-table.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Space, Table } from "antd";
 import type { TableProps } from "antd";
 import { IUser } from "@/types/user";
+import { usePathname, useSearchParams } from "next/navigation";
 
 const columns: TableProps<IUser>["columns"] = [
   {
@@ -34,15 +35,42 @@ const columns: TableProps<IUser>["columns"] = [
 ];
 
 const UserTable: React.FC<{ users: IUser[] }> = ({ users }) => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+
+  const handlePagination = (
+    pagination: any,
+    filters: any,
+    sorter: any,
+    extra: any
+  ) => {
+    if (pagination && pagination.current) {
+      const params = new URLSearchParams(searchParams);
+      params.set("page", pagination.current);
+      replace(`${pathname}?${params.toString()}`);
+      setIsFetching(true);
+    }
+  };
+
   return (
     <div>
       <Table
+        rowKey={"id"}
         columns={columns}
         dataSource={users}
+        onChange={handlePagination}
         pagination={{
           current: 1,
-          pageSize: 5,
+          pageSize: 10,
           total: users.length,
+          showTotal: (total, range) => {
+            return (
+              <div>
+                {" "}
+                {range[0]}-{range[1]} trên {total} rows
+              </div>
+            );
+          },
         }}
       />
     </div>
